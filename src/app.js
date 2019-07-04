85853d451ef09e3b80885762e3c524fb74296626
RenatoSpll <renatomacedo500@gmail.com>
2019-07-03 22:54:52 -0300

    env etc

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.listen(process.env.PORT, () => {
     if (message.content.startsWith('?play')) {
       const search = message.content.split('?play')[1];
 
-      axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${search}&key=${YOUTUBE_API_KEY}`).then((response) => {
+      axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${search}&key=${process.env.YOUTUBE_API_KEY || YOUTUBE_API_KEY}`).then((response) => {
         console.log(response.data);
         const { videoId } = response.data.items[0].id;
 
@@ -45,7 +45,7 @@ app.listen(process.env.PORT, () => {
     }
   });
 
-  client.login(BOT_TOKEN);
+  client.login(process.env.BOT_TOKEN || BOT_TOKEN);
 });
 
 app.get('/', (req, res) => {
