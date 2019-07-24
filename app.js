f390b1b0e09812588c95711017b12cbf44d9c5e7
RenatoSpll <renatomacedo500@gmail.com>
2019-07-24 01:14:30 -0300

    server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 
+const express = require('express');
 const axios = require('axios');
 const ytdl = require('ytdl-core');
 const { Client } = require('discord.js');
@@ -7,42 +8,42 @@ const { Client } = require('discord.js');
 
 // const { YOUTUBE_API_KEY, BOT_TOKEN } = require('./src/config.json');
 
+const app = express();
 const client = new Client();
 
+// let nowPlaying = '';
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
+app.listen(process.env.PORT, () => {
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+    client.on('message', async (message) => {
+      if (message.content.startsWith('?play')) {
+        const search = message.content.split('?play')[1];
+        axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${search}&key=${process.env.YOUTUBE_API_KEY}`).then((response) => {
 
-client.on('message', async (message) => {
-  if (message.content.startsWith('?play')) {
-    const search = message.content.split('?play')[1];
+          const { videoId } = response.data.items[0].id;
 
-    axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${search}&key=${process.env.YOUTUBE_API_KEY}`).then((response) => {
-      console.log(response.data);
-      const { videoId } = response.data.items[0].id;
-
-      message.channel.send(`Now playing ${response.data.items[0].snippet.title}`);
-
-      message.member.voice.channel.join().then((connection) => {
-        connection.play(ytdl(`https://www.youtube.com/watch?v=${videoId}`, { filter: 'audioonly' })).on('finish', () => {
-          message.channel.send('Song Finished');
+          message.channel.send(`Now playing ${response.data.items[0].snippet.title}`);
+          message.member.voice.channel.join().then((connection) => {
+            connection.play(ytdl(`https://www.youtube.com/watch?v=${videoId}`, { filter: 'audioonly' })).on('finish', () => {
+              message.channel.send('Song Finished');
+            });
+          }).catch((err) => {
+            console.log(err);
+          });
+        }).catch((err) => {
+          console.log(err.response.data);
         });
-      }).catch((err) => {
-        console.log(err);
-      });
-    }).catch((err) => {
-      console.log(err.response.data);
+      }
+
+      if (message.content.startsWith('?leave')) {
+        message.member.voice.channel.leave();
+      }
+
+      if (message.content.startsWith('?AYT')) {
+        message.channel.send('Yep');
+      }
     });
-  }
-
-  if (message.content.startsWith('?leave')) {
-    message.member.voice.channel.leave();
-  }
-
-  if (message.content.startsWith('?AYT')) {
-    message.channel.send('Yep');
-  }
+    client.login(process.env.BOT_TOKEN);
+  });
 });
-
-client.login(process.env.BOT_TOKEN);
